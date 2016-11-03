5e7c440bbff370df36a65ebf925152279066a22d
Clement Allen <clementallen313@gmail.com>
2016-11-03 10:19:58 +0000

    Added more ordering to flight api

--- routes/api/get.js
+++ routes/api/get.js
@@ -6,7 +6,7 @@ var Flight = require('../../models/Flight');
 api.route('/flights')
 
     .get(function(req, res) {
-        Flight.find().sort({ date: 1 }).exec(function(err, flights) {
+        Flight.find().sort({ date: 1, takeoffTime: 1}).exec(function(err, flights) {
             if(err) {
                 res.status(500);
                 res.json({
